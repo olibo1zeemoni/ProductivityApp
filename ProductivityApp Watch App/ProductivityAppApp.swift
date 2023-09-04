c93ab3d3336b9aa9a20d572fa1579d76da1fa2e1
George Boahene <olibo1zeemoni13@gmail.com>
2023-09-04 10:18:21 +0000

    Initial Commit

---
+++ ProductivityApp Watch App/ProductivityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityAppApp.swift
+//  ProductivityApp Watch App
+//
+//  Created by Olibo moni on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
